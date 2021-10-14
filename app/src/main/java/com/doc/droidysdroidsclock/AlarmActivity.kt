a35616cf248e723731583d3df9570ac5ab5f7cdd
Rojae Martin <rojaedmartin@gmail.com>
2021-10-13 19:52:09 -0500

    Initial mockup of clock page and intent setup for alarm page

---
+++ app/src/main/java/com/doc/droidysdroidsclock/AlarmActivity.kt
@@ -0,0 +1,12 @@
+package com.doc.droidysdroidsclock
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AlarmActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm)
+    }
+}
--- app/src/main/java/com/doc/droidysdroidsclock/MainActivity.kt
+++ app/src/main/java/com/doc/droidysdroidsclock/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.doc.droidysdroidsclock
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextClock
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
+import java.text.DateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val alarmTab: Button = findViewById(R.id.button2)
+        alarmTab.setOnClickListener {
+            Intent(this, AlarmActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
+        val calendar: Calendar = Calendar.getInstance()
+        val currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.time)
+        val textViewDate: TextView = findViewById(R.id.dateText)
+        textViewDate.text = currentDate.toString().lowercase()
+
+        val c: Context = applicationContext
+        val timeClock: TextClock = findViewById(R.id.digitalClock)
+        val typeface = ResourcesCompat.getFont(c,R.font.comfortaa_bold)
+        timeClock.typeface = typeface
     }
 }
